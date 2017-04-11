c503181bba9e584990c1ed9d7df3f2ca78988fc5
Rachel Arkebauer <rarkebauer@gmail.com>
2017-04-11 18:42:00 -0400

    added testing automation and get client ip address

---
+++ index.js
@@ -0,0 +1,36 @@
+var Koa = require("koa");
+var locale = require('koa-locale');
+var app =  module.exports = new Koa();
+
+locale(app, {
+  // the `lang-key` defaults to `lang`
+  lang: 'language'
+});
+
+app.use(async (ctx) => {
+	try{
+		ctx.body = 'Hello Worl';
+		console.log(ctx)
+
+		var langu = ctx.request.acceptsLanguages();
+		console.log(langu);
+
+		var clientIp = ctx.request.ip;
+		console.log(clientIp);
+
+		var lang = ctx.language;
+		console.log(lang);
+		
+	} catch(err) {
+		ctx.body = { message: err.message };
+		ctx.status = err.status || 500;
+	} 
+});
+
+
+var port = process.env.PORT || (process.argv[2] || 3000);
+port = (typeof port === "number") ? port : 3000;
+
+if(!module.parent){ app.listen(port); }
+
+console.log("Application started. Listening on port: " + port);
